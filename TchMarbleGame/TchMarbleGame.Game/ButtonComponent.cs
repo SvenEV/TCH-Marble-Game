a64a4956fb39a091f069cc9620e68260789407ff
SvenEV <sven@vinkemeier.de>
2016-11-25 11:06:02 +0100

    Button progress

---
+++ TchMarbleGame/TchMarbleGame.Game/ButtonComponent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SiliconStudio.Core.Mathematics;
+using SiliconStudio.Xenko.Input;
+using SiliconStudio.Xenko.Engine;
+
+namespace TchMarbleGame
+{
+    public class ButtonComponent : AsyncScript
+    {
+        // Declared public member fields and properties will show in the game studio
+
+        public override async Task Execute()
+        {
+            while(Game.IsRunning)
+            {
+                // Do stuff every new frame
+                await Script.NextFrame();
+            }
+        }
+    }
+}

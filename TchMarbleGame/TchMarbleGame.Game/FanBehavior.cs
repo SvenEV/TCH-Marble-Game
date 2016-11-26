9af749bfe9f0e44be3c1916d266d2f60b52c85f8
Sven Vinkemeier <sven@vinkemeier.de>
2016-11-26 08:57:23 +0100

    Fan can now be enabled and disabled

--- TchMarbleGame/TchMarbleGame.Game/FanBehavior.cs
+++ TchMarbleGame/TchMarbleGame.Game/FanBehavior.cs
@@ -20,6 +20,11 @@ namespace TchMarbleGame
         /// </summary>
         public float AppliedForce { get; set; } = 3;
 
+        /// <summary>
+        /// Use this to activate and deactivate the fan.
+        /// </summary>
+        public bool IsEnabled { get; set; }
+
         public override void Start()
         {
             Script.AddTask(HandleNewCollisions);
@@ -54,23 +59,24 @@ namespace TchMarbleGame
 
         public override void Update()
         {
+            if (!IsEnabled)
+                return;
+
+            // Calculate direction of fan
+            var fanDirection = Vector3.UnitY;
+            Vector3 worldPosition, worldScale;
+            Quaternion worldRotation;
+            ImpactArea.Entity.Transform.WorldMatrix.Decompose(out worldPosition, out worldRotation, out worldScale);
+            worldRotation.Rotate(ref fanDirection);
+
             foreach (var rigidbody in _entitiesInRange)
             {
                 var distance = Vector3.Distance(Entity.Transform.Position, rigidbody.Entity.Transform.Position);
                 var force = AppliedForce * (10 - distance);
-
-                var fanDirection = Vector3.UnitY;
-
-                Vector3 worldPosition, worldScale;
-                Quaternion worldRotation;
-
-                ImpactArea.Entity.Transform.WorldMatrix.Decompose(out worldPosition, out worldRotation, out worldScale);
-                worldRotation.Rotate(ref fanDirection);
-
                 rigidbody.ApplyForce(force * fanDirection);
             }
 
-            Entity.Transform.Rotation *= Quaternion.RotationY(1);
+            Entity.Get<RigidbodyComponent>().ApplyTorque(fanDirection);
         }
     }
 }
